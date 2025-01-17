068141c9bb57e555716efd687302687447032bd2
obchain <riteshnikhoriya94@gmail.com>
2025-01-17 16:53:04 +0530

    added hypernative pool

---
+++ contracts/protocol/pool/PoolHypernative.sol
@@ -0,0 +1,124 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.12;
+
+import {IPoolAddressesProvider} from '../../interfaces/IPoolAddressesProvider.sol';
+import {IPool} from '../../interfaces/IPool.sol';
+import {Pool} from './Pool.sol';
+import {DataTypes} from '../libraries/types/DataTypes.sol';
+import {SupplyLogic} from '../libraries/logic/SupplyLogic.sol';
+import {BorrowLogic} from '../libraries/logic/BorrowLogic.sol';
+import {OracleProtected} from 'contracts/dependencies/firewall/OracleProtected.sol';
+
+contract PoolHypernative is Pool, OracleProtected {
+  /**
+   * @dev Constructor.
+   * @param provider The address of the PoolAddressesProvider contract
+   */
+  constructor(IPoolAddressesProvider provider) payable Pool(provider) {
+    // Intentionally left blank
+  }
+
+  function initialize(
+    address _oracle,
+    address admin,
+    IPoolAddressesProvider provider
+  ) public override initializer {
+    __OracleProtected_init(_oracle, admin);
+    super.initialize(provider);
+  }
+
+  /// @inheritdoc IPool
+  function supply(
+    address asset,
+    uint256 amount,
+    address onBehalfOf,
+    uint16 referralCode
+  ) public override onlyOracleApproved {
+    SupplyLogic.executeSupply(
+      _reserves,
+      _reservesList,
+      _usersConfig[onBehalfOf],
+      DataTypes.ExecuteSupplyParams({
+        asset: asset,
+        amount: amount,
+        onBehalfOf: onBehalfOf,
+        referralCode: referralCode
+      })
+    );
+  }
+
+  /// @inheritdoc IPool
+  function withdraw(
+    address asset,
+    uint256 amount,
+    address to
+  ) public override onlyOracleApproved returns (uint256) {
+    return
+      SupplyLogic.executeWithdraw(
+        _reserves,
+        _reservesList,
+        _eModeCategories,
+        _usersConfig[msg.sender],
+        DataTypes.ExecuteWithdrawParams({
+          asset: asset,
+          amount: amount,
+          to: to,
+          reservesCount: _reservesCount,
+          oracle: ADDRESSES_PROVIDER.getPriceOracle(),
+          userEModeCategory: _usersEModeCategory[msg.sender]
+        })
+      );
+  }
+
+  /// @inheritdoc IPool
+  function borrow(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode,
+    uint16 referralCode,
+    address onBehalfOf
+  ) public override onlyOracleApproved {
+    BorrowLogic.executeBorrow(
+      _reserves,
+      _reservesList,
+      _eModeCategories,
+      _usersConfig[onBehalfOf],
+      DataTypes.ExecuteBorrowParams({
+        asset: asset,
+        user: msg.sender,
+        onBehalfOf: onBehalfOf,
+        amount: amount,
+        interestRateMode: DataTypes.InterestRateMode(interestRateMode),
+        referralCode: referralCode,
+        releaseUnderlying: true,
+        maxStableRateBorrowSizePercent: _maxStableRateBorrowSizePercent,
+        reservesCount: _reservesCount,
+        oracle: ADDRESSES_PROVIDER.getPriceOracle(),
+        userEModeCategory: _usersEModeCategory[onBehalfOf],
+        priceOracleSentinel: ADDRESSES_PROVIDER.getPriceOracleSentinel()
+      })
+    );
+  }
+
+  /// @inheritdoc IPool
+  function repay(
+    address asset,
+    uint256 amount,
+    uint256 interestRateMode,
+    address onBehalfOf
+  ) public override onlyOracleApproved returns (uint256) {
+    return
+      BorrowLogic.executeRepay(
+        _reserves,
+        _reservesList,
+        _usersConfig[onBehalfOf],
+        DataTypes.ExecuteRepayParams({
+          asset: asset,
+          amount: amount,
+          interestRateMode: DataTypes.InterestRateMode(interestRateMode),
+          onBehalfOf: onBehalfOf,
+          useATokens: false
+        })
+      );
+  }
+}
